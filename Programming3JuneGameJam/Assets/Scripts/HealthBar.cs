59b56180cd3a659ab376a1338c72f6d82bdec7ec
Gianni Vallerini <gianni.vallerini@hotmail.com>
2020-06-13 12:57:38 +0200

    added singleton utilities and health bar behaviour

--- Programming3JuneGameJam/Assets/Scripts/HealthBar.cs
+++ Programming3JuneGameJam/Assets/Scripts/HealthBar.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using Sirenix.OdinInspector;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Title("References")]
+    [SerializeField] private Slider slider = null;
+    [SerializeField] private Image fill = null;
+    [SerializeField] private Image background = null;
+    [SerializeField] private SliderColors[] sliderColors = null;
+
+    [Space(5), Title("Debugging")]
+    [SerializeField] private bool isDebugging = false;
+    [SerializeField] private float debugCurrentHp = 10;
+    [SerializeField] private float debugMaxHp = 20;
+
+    private void Update()
     {
-        
+        if (isDebugging)
+        {
+            SetValue(debugCurrentHp, debugMaxHp);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetValue(float currValue, float maxValue)
     {
-        
+        float normalizedValue = (float)(currValue - .001) % maxValue;
+        slider.value = normalizedValue / maxValue;
+        UpdateColor(currValue, maxValue);
+    }
+
+    private void UpdateColor(float currValue, float maxValue)
+    {
+        var numberOfBars = (int)((currValue - .001) / maxValue);
+        if(numberOfBars >= sliderColors.Length)
+        {
+            numberOfBars = sliderColors.Length - 1;
+        }
+
+        fill.color = sliderColors[numberOfBars].fillColor;
+        background.color = sliderColors[numberOfBars].backgroundColor;
+    }
+
+    [System.Serializable]
+    public struct SliderColors
+    {
+        public Color fillColor;
+        public Color backgroundColor;
     }
 }
---
+++ Programming3JuneGameJam/Assets/Scripts/Utilities/SingletonDDOL.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+public abstract class SingletonDDOL<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T instance;
+
+    public virtual void Awake()
+    {
+        if (!instance)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+public abstract class SerializedSingletonDDOL<T> : SerializedMonoBehaviour where T : SerializedMonoBehaviour
+{
+    public static T instance;
+
+    public virtual void Awake()
+    {
+        if (!instance)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
