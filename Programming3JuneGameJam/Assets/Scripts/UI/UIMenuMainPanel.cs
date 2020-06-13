faa1004420484759438497f0b4a048583f961574
Gianni Vallerini <gianni.vallerini@hotmail.com>
2020-06-13 13:39:33 +0200

    created basic menu, UIMenuButton, added MainMenu and MainScene to Build setting scenes

---
+++ Programming3JuneGameJam/Assets/Scripts/UI/UIMenuMainPanel.cs
@@ -0,0 +1,30 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMenuMainPanel : MonoBehaviour
+{
+    [Title("Play")]
+    [SerializeField] private UIMenuButton playButton;
+    [SerializeField] private string playScene;
+    [Space(5), Title("Quit")]
+    [SerializeField] private UIMenuButton quitButton;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(Play);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    private void Play()
+    {
+        SceneManager.LoadSceneAsync(playScene);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+}
