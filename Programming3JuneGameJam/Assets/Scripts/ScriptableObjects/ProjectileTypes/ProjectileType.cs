c7a1d366f5a0186188197cd9994a39974791206a
Michele Santoni <mikysanto.97@gmail.com>
2020-06-13 18:04:40 +0200

    [TECH] added ProjectileType SO; created test_projectile (ProjectileType);

---
+++ Programming3JuneGameJam/Assets/Scripts/ScriptableObjects/ProjectileTypes/ProjectileType.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "projectile_type", menuName = "Shooting/ProjectileType", order = 0)]
+public class ProjectileType : ScriptableObject
+{
+    #region Fields
+
+    public GameObject prefab;
+    public float damage = 10f;
+    public float speed = 20f;
+    public float lifeTime = 5f;
+
+    #endregion
+
+    #region Methods
+    
+    public virtual ProjectileControl SpawnProjectile(Vector3 spawnPoint)
+    {
+        var instance = Instantiate(prefab, spawnPoint, Quaternion.identity);
+        return instance.GetComponent<ProjectileControl>();
+    }
+
+    public virtual void ProjectileBehaviour()
+    {}
+
+    #endregion
+}
