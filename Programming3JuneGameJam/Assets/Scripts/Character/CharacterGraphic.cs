eafb99b88e025713a1e15664b47c2869baf7f1a5
Michele Santoni <mikysanto.97@gmail.com>
2020-06-15 00:43:15 +0200

    [TECH] fixed bug wronge reference;

--- Programming3JuneGameJam/Assets/Scripts/Character/CharacterGraphic.cs
+++ Programming3JuneGameJam/Assets/Scripts/Character/CharacterGraphic.cs
@@ -38,7 +38,7 @@ public class CharacterGraphic : MonoBehaviour
         var instance = Instantiate(character.Stats.weaponPrefab, weaponHandler);
         instance.transform.localPosition = Vector3.zero;
         instance.transform.localRotation = Quaternion.Euler(Vector3.zero);
-        character.animation.SetWeaponType(character.Stats.weaponType);
+        character.Animation.SetWeaponType(character.Stats.weaponType);
     }
 
     public void SetActive(bool isActive)
